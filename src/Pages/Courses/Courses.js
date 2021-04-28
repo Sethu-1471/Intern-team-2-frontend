8c7eafc5dd547400796f65912130846dcab0be6c
Dhanavel <dhanavel@codingmart.com>
2021-04-28 11:33:29 +0530

    ADDED GRID COURSES ITEMS IN COURSES PAGE WITH MOBILE RESPONSIVENESS

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Courses.css'
 import Header from '../../components/Header/Header'
+import CoursesList from '../../components/CoursesList/CoursesList'
 export default function Courses() {
     return (
         <>
             <Header/>
+            <CoursesList/>
         </>
     )
 }
