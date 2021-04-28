4079df647d6019f3e66fec6c0f106e097d7c35fa
Dhanavel <dhanavel@codingmart.com>
2021-04-28 10:53:12 +0530

    ADDED HEADER IN COURSES PAGE USING MATERIAL DESIGN UI

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Courses.css'
+import Header from '../../components/Header/Header'
 export default function Courses() {
     return (
-        <div>
-            Courses
-        </div>
+        <>
+            <Header/>
+        </>
     )
 }
