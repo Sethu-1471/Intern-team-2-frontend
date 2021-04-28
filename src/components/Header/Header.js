8a9e21fc09044c0bb9e632c37dedd480b594c854
Dhanavel <dhanavel@codingmart.com>
2021-04-28 11:07:16 +0530

    MADE THE HEADER MOBILE RESPONSIVE

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -78,45 +78,16 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
   const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
   const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
     <Menu
@@ -128,32 +99,34 @@ export default function Header() {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      {/* <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
+      <MenuItem onClick={handleMobileMenuClose}>
+        <IconButton color="inherit">
+            <LibraryBooksIcon />
+            <h6 style={{fontWeight: 500,fontSize: '16px',letterSpacing: '0.05em',marginLeft: '4px',marginBottom:'0'}}>
+                Your Courses
+            </h6>
         </IconButton>
-        <p>Messages</p>
-      </MenuItem> */}
-      {/* <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem> */}
-      <MenuItem onClick={handleProfileMenuOpen}>
+      </MenuItem>
+      <MenuItem onClick={handleMobileMenuClose}>
         <IconButton
-          aria-label="account of current user"
           aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
           color="inherit"
         >
-          <AccountCircle />
+            <AccountCircle />
+            <h6 style={{fontWeight: 500,fontSize: '16px',letterSpacing: '0.05em',marginLeft: '4px',marginBottom:'0'}}>
+                Profile
+            </h6>
         </IconButton>
-        <p>Profile</p>
+      </MenuItem>
+      <MenuItem onClick={handleMobileMenuClose}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge color="secondary">
+                <LockIcon />
+                <h6 style={{fontWeight: 500,fontSize: '16px',letterSpacing: '0.05em',marginLeft: '4px',marginBottom:'0'}}>
+                    Logout
+                </h6>
+              </Badge>
+            </IconButton>
       </MenuItem>
     </Menu>
   );
@@ -162,14 +135,6 @@ export default function Header() {
     <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          {/* <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography className={classes.title} variant="h6" noWrap>
             LOGO
           </Typography>
@@ -197,7 +162,6 @@ export default function Header() {
             <IconButton
               edge="end"
               aria-controls={menuId}
-              onClick={handleProfileMenuOpen}
               color="inherit"
             >
                 <AccountCircle />
