c7c0471db3d5be46c03e70be7f70b521c5ff2153
balaji <balaji443.bk@gmail.com>
2021-04-29 19:52:09 +0530

    Login page Implemented and refactored Registerpage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import { Switch, Route } from "react-router-dom";
 import Courses from "./Pages/Courses/Courses";
 import Home from "./Pages/Home/Home";
 import AdminPage from "./Pages/Admin/Admin";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
     return (
         <>
             <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
                 <Route path="/" exact>
                     <Home />
                 </Route>
