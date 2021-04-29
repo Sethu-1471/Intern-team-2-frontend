be0e337d6f1826ffd0369d2d4849a5977bc58507
Sethu-1471 <rpsethu1471@gmail.com>
2021-04-29 20:01:26 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import Landing from "./components/Landing/Landing";
 import CourseAddMainPage from "./components/CourseCreate/MainPageOne";
 import CourseEditSubPage from "./components/CourseCreate/MainPageTwo";
 import { ToastContainer } from "react-toastify";
@@ -11,12 +10,12 @@ function App() {
   return (
     <>
       <ToastContainer />
+      <Header />
       <Switch>
-        <Route path="/" exact component={Landing} />
+        {/* <Route path="/" exact component={Landing} /> */}
         {/* <Route path="/courses" exact>
           <Courses />
         </Route> */}
-        <Header />
         <Route path="/addcourse" exact component={CourseAddMainPage} />
         <Route
           path={`/editsubcontent/:id`}
